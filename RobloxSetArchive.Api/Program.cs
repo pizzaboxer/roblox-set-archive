c7a5964823e7d146dc6bebdaa4718ca36ce2b7a7
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-08-14 16:14:29 +0100

    Expose Swagger definition publicly

--- RobloxSetArchive.Api/Program.cs
+++ RobloxSetArchive.Api/Program.cs
@@ -20,11 +20,15 @@ builder.Services.AddHttpClient("Roblox", httpClient =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+// }
 
 app.Use(async (context, next) => 
 {
