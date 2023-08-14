f9f52892ecdb3dc425cc3f895f54f723cfec59d4
pizzaboxer <pizzaboxer@pizzaboxer.xyz>
2023-08-14 17:08:45 +0100

    Add publish profile, fix Swagger, disable HTTPS

--- RobloxSetArchive.Api/Program.cs
+++ RobloxSetArchive.Api/Program.cs
@@ -25,7 +25,7 @@ var app = builder.Build();
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
         options.RoutePrefix = string.Empty;
     });
 // }
